ab346de7cdb373d2c3b940965163873aa65b9a27
Ayush vashisth <ayushvashisth@Blanche.local>
2026-08-23 19:42:50 +0530

    added cors

--- Server/app.js
+++ Server/app.js
@@ -38,6 +38,7 @@ const app = express();
 
 const allowedOrigins = [
   process.env.CLIENT_URL,
+  "https://jandrishti.aalsicoders.in",
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://localhost:3000",
